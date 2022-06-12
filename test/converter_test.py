f4d0cad79bc75ee064090cccac4906f3396361d2
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2022-06-13 01:37:23 +0200

    Added test for printing multiple lines

--- test/converter_test.py
+++ test/converter_test.py
@@ -9,5 +9,10 @@ def test_helloworld() -> None:
     assert run_example("helloworld") == "Hello, world!\n\n"
 
 
+def test_multiple_lines() -> None:
+    text = ["First", "Second", "Third"]
+    expected = "\n\n".join([i + " line" for i in text]) + "\n\n"
+    assert run_example("print_few_lines") == expected
+
 def test_primary() -> None:
     assert run_example("if_primary_number") == "11 is primary\n"
