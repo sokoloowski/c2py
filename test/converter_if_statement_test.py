678c64c43476ce3e0917df58937f6fcbe5c96601
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2022-06-13 10:33:37 +0200

    Updated tests

--- test/converter_if_statement_test.py
+++ test/converter_if_statement_test.py
@@ -2,20 +2,20 @@ from .helper import run_example
 
 
 def test_extended_if_statement() -> None:
-    assert run_example("if_statements/extended_if_statement") == "4\n"
+    assert run_example("if_statements/extended_if_statement") == "4"
 
 
 def test_if_elif_else() -> None:
-    assert run_example("if_statements/if_elif_else") == "false\n"
+    assert run_example("if_statements/if_elif_else") == "false"
 
 
 def test_if_statement_with_logical_expression() -> None:
-    assert run_example("if_statements/if_statement_with_logical_expression") == "true\n"
+    assert run_example("if_statements/if_statement_with_logical_expression") == "true"
 
 
 def test_simple_if_else() -> None:
-    assert run_example("if_statements/simple_if_else") == "true\n"
+    assert run_example("if_statements/simple_if_else") == "true"
 
 
 def test_simple_if() -> None:
-    assert run_example("if_statements/simple_if") == "true\n"
+    assert run_example("if_statements/simple_if") == "true"
--- test/converter_test.py
+++ test/converter_test.py
@@ -6,13 +6,13 @@ def test_empty() -> None:
 
 
 def test_helloworld() -> None:
-    assert run_example("helloworld") == "Hello, world!\n\n"
+    assert run_example("helloworld") == "Hello, world!\n"
 
 
 def test_multiple_lines() -> None:
     text = ["First", "Second", "Third"]
-    expected = "\n\n".join([i + " line" for i in text]) + "\n\n"
+    expected = "\n".join([i + " line" for i in text]) + "\n"
     assert run_example("print_few_lines") == expected
 
 def test_primary() -> None:
-    assert run_example("if_primary_number") == "11 is primary\n"
+    assert run_example("if_primary_number") == "11 is primary"
